f70f89b246eba7737bf35602fed6b536902f2897
Sam Yuan <samwise.yuan@gmail.com>
2026-03-20 01:19:52 +0800

    feat: support CF_PAGES dual deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/sanono-report/',
+  base: process.env.CF_PAGES ? '/' : '/sanono-report/',
   assetsInclude: ['**/*.md'],
 })
